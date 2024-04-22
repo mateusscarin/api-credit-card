4581a1c3ff130b02035e0362336b0c51a9c5dafb
mateusscarin <mateus.scarin@gmail.com>
2024-04-22 20:34:58 -0300

    feat: mapeamento das rotas

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { CreateCardController } from "./controllers/card/CreateCardController";
+import { ListCardsByUserController } from "./controllers/card/ListCardsByUserController";
+import { ValidateCardController } from "./controllers/card/ValidateCardController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./services/user/middlewares/IsAuthenticated";
+
+const router: Router = Router();
+
+router.post('/user', new CreateUserController().handle);
+router.post('/session', new AuthUserController().handle);
+router.get('/userinfo', isAuthenticated, new DetailUserController().handle);
+router.post('/card', isAuthenticated, new CreateCardController().handle);
+router.get('/card/:cardNumber', isAuthenticated, new ValidateCardController().handle);
+router.get('/user/cards', isAuthenticated, new ListCardsByUserController().handle);
+
+export { router };
+
